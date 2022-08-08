8e0959ff4274097aa6280e935c6f54320bd7738b
it20278380 <it20278380@my.sliit.lk>
2022-08-08 10:48:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { CheckLisence } from '@pamuditha-blockstars/check-license-react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const result = CheckLisence(process.env.REACT_APP_LICENSE_KEY);
+  console.log("License key: " + process.env.REACT_APP_LICENSE_KEY);
+  console.log(result);
 }
 
 export default App;
